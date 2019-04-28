493aca46f39b09ff012a93118d6356796711c05b
Noah <franck.noahk@gmail.com>
2019-04-28 16:25:07 -0500

    Initial commit

---
+++ app/src/main/java/com/noahfranck/swoosh/WelcomeToTheTeam.kt
@@ -0,0 +1,12 @@
+package com.noahfranck.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeToTheTeam : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_to_the_team)
+    }
+}
