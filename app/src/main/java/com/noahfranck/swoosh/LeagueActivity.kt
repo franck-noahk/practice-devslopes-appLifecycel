35ac09efac6975213995b6f5d930fa17a52ddbd9
Noah F <franck.noahk@gmail.com>
2019-05-21 22:18:16 -0500

    Different way of doing on click listener

--- app/src/main/java/com/noahfranck/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/noahfranck/swoosh/LeagueActivity.kt
@@ -2,11 +2,16 @@ package com.noahfranck.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun LeagueNextClicked (view: View) {
+
     }
 }
