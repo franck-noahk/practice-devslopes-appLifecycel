4a6cd5dca15ac47d199ba6ae1a865159e108e419
Noah F <franck.noahk@gmail.com>
2019-05-21 21:18:55 -0500

    Filled in log statements
    Each stage of the activity lifecycle now has a log statement that tells what happens when it is run.

--- app/src/main/java/com/noahfranck/swoosh/BasicActivity.kt
+++ app/src/main/java/com/noahfranck/swoosh/BasicActivity.kt
@@ -2,10 +2,38 @@ package com.noahfranck.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class BasicActivity : AppCompatActivity() {
 
+    val TAG = "LifeCycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"${javaClass.simpleName} On Create" )
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "${javaClass.simpleName} On restart")
+    }
+    override fun onDestroy() {
+        Log.d(TAG,"${javaClass.simpleName} On Destroy" )
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+
+        Log.d(TAG,"${javaClass.simpleName} On resume" )
+        super.onResume()
+    }
+    override fun onStart() {
+        Log.d(TAG,"${javaClass.simpleName} On start" )
+        super.onStart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} on Pause")
+        super.onPause()
     }
 }
