b7bf08a7307a3afca5372064e498c3b88f628209
Noah F <franck.noahk@gmail.com>
2019-05-22 15:46:51 -0500

    Added function for buttons pressed
    Added a function that is called if either button detects a toggle and runs the command accordingly.

--- app/src/main/java/com/noahfranck/swoosh/Skill_Level.kt
+++ app/src/main/java/com/noahfranck/swoosh/Skill_Level.kt
@@ -2,13 +2,30 @@ package com.noahfranck.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_skill_level.*
 
 class Skill_Level : AppCompatActivity() {
 
     var league = ""
+    var skillLevel = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill_level)
         league = intent.getStringExtra(EXTRA_LEAGUE)
+        NoobToggle.setOnCheckedChangeListener { buttonView, isChecked -> buttonPressed("noob") }
+        BallerToggle.setOnCheckedChangeListener { buttonView, isChecked -> buttonPressed("baller") }
     }
+
+    fun buttonPressed(button : String){
+        if (button == "noob"){
+            BallerToggle.setChecked(false)
+            skillLevel = "Beginner"
+        }else if (button == "baller"){
+            NoobToggle.setChecked(false)
+            skillLevel  = "Baller"
+        }
+    }
+
 }
