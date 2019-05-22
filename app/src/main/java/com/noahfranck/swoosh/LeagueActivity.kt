f6f002bc7cd763f7711434185bff12d78cabefb8
Noah F <franck.noahk@gmail.com>
2019-05-22 08:35:05 -0500

    Set up league logic so that only one can be selected.

--- app/src/main/java/com/noahfranck/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/noahfranck/swoosh/LeagueActivity.kt
@@ -4,16 +4,48 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BasicActivity() {
+
+    var womonButtonPressed = false
+    var menButtonPressed = false
+    var coedButtonPressed = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        women_toggle.setOnClickListener {
+            if (menButtonPressed || coedButtonPressed){
+                menButtonPressed = false
+                coedButtonPressed = false
+            }
+            womonButtonPressed = true
+        }
+
+        co_ed_toggle.setOnClickListener {
+            if (menButtonPressed || womonButtonPressed) {
+                menButtonPressed = false
+                womonButtonPressed = false
+            }
+        }
     }
 
     fun LeagueNextClicked (view: View) {
-        val skillActivity = Intent(this,Skill_Level::class.java)
-        startActivity(skillActivity)
+        if (menButtonPressed || womonButtonPressed || coedButtonPressed) {
+            val skillActivity = Intent(this, Skill_Level::class.java)
+            startActivity(skillActivity)
+        }
     }
+
+    fun menToggleClicked(view: View){
+        if (womonButtonPressed || coedButtonPressed){
+            womonButtonPressed = false
+            coedButtonPressed = false
+        }
+        menButtonPressed = true
+    }
+
+
 }
