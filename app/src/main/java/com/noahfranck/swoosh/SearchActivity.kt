6e97cbb8379ecc2b10f8365332bb9033b6f11b0e
Noah F <franck.noahk@gmail.com>
2019-05-23 13:10:39 -0500

    pulling in the intents
    This should assign the intents to the local variables and then assigned them to the string.

--- app/src/main/java/com/noahfranck/swoosh/SearchActivity.kt
+++ app/src/main/java/com/noahfranck/swoosh/SearchActivity.kt
@@ -2,11 +2,18 @@ package com.noahfranck.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        var leage = intent.getStringExtra(EXTRA_LEAGUE)
+        var skill = intent.getStringExtra(EXTRA_SKILL)
+
+        SearchLeaguesText.text = "Looking for a $leage $skill league near you..."
     }
 }
