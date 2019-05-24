bfed2bb290299835f496e84c60df5c8f7833404d
Noah F <franck.noahk@gmail.com>
2019-05-24 13:37:39 -0500

    Added Save instance to League
    This will save the instance of the player when it is rotated.

--- app/src/main/java/com/noahfranck/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/noahfranck/swoosh/LeagueActivity.kt
@@ -66,4 +66,9 @@ class LeagueActivity : BasicActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER,player)
+    }
+
 }
